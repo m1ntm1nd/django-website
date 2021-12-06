e58ffa3e13c53d47de48a73b4b7896981efbec6f
Pavel Kulagin <kulagin.pavel.unn@gmail.com>
2021-12-06 17:38:03 +0300

    db relations + index2 out

--- studhack/models.py
+++ studhack/models.py
@@ -4,26 +4,33 @@ from django.db import models
 #Table: tasks
 
 class Tasks(models.Model):
-    id = models.IntegerField(primary_key=True, unique=True)
+    #id = models.IntegerField(primary_key=True, unique=True)
     task_name = models.CharField(max_length=255)
     expert = models.CharField(max_length=255)
     #description = models.TextField()
     #stack = models.TextField()
     #photo = models.ImageField(upload_to=f"photos/{id}")
-    #participants = One to Many        
+    #participants = One to Many
+    def __str__(self):
+        return self.title
+
+    # def get_absolute_url(self):
+    #     return reverse('post', kwargs={'post_id': self.pk})        
 
 class Teams(models.Model):
     id = models.IntegerField(primary_key=True, unique=True)
     team_name = models.CharField(max_length=255)
     #task
     #team_photo = models.ImageField(upload_to=f"photos/{id}")
-    # team_members - One to Many
+    
     # team_captain - One to One ?
+    def __str__(self):
+        return self.team_name
 
 class Participants(models.Model):
     full_name = models.CharField(max_length=255)
     #password?
-    team = models.CharField(max_length=255, default=None, null=True) #one to Many?
+    team = models.ForeignKey('Teams', on_delete=models.PROTECT, null=True) #one to Many?
 
-
-    
+    def __str__(self):
+        return self.full_name
