79489f3f0dbcf0eb5fd646359c857640f3e2d4b0
Pavel Kulagin <kulagin.pavel.unn@gmail.com>
2021-12-06 16:38:34 +0300

    created dbases for all categories

--- studhack/models.py
+++ studhack/models.py
@@ -4,12 +4,26 @@ from django.db import models
 #Table: tasks
 
 class Tasks(models.Model):
-    id = 0
+    id = models.IntegerField(primary_key=True, unique=True)
     task_name = models.CharField(max_length=255)
-    #sphere
     expert = models.CharField(max_length=255)
-    description = models.TextField()
-    stack = models.TextField()
-    photo = models.ImageField(upload_to=f"photos/{id}")        
+    #description = models.TextField()
+    #stack = models.TextField()
+    #photo = models.ImageField(upload_to=f"photos/{id}")
+    #participants = One to Many        
+
+class Teams(models.Model):
+    id = models.IntegerField(primary_key=True, unique=True)
+    team_name = models.CharField(max_length=255)
+    #task
+    #team_photo = models.ImageField(upload_to=f"photos/{id}")
+    # team_members - One to Many
+    # team_captain - One to One ?
+
+class Participants(models.Model):
+    full_name = models.CharField(max_length=255)
+    #password?
+    team = models.CharField(max_length=255, default=None, null=True) #one to Many?
+
 
     
