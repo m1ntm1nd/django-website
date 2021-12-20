38eb1d794c13c6cabfe116bf1b11034d7de71b0c
Pavel Kulagin <kulagin.pavel.unn@gmail.com>
2021-12-20 13:44:28 +0300

    asd
    das

--- sh_website/settings.py
+++ sh_website/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%7g90gh(n33$dn0e29tp#oz^5v8@4_ln__dlyrzmv2yqgm0q$g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
